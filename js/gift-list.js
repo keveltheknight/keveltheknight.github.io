b68a127a70a70737e289e14c27830123b8eb705e
kevelTheKnight <kevinbcosta@gmail.com>
2023-05-10 02:10:10 -0300

    wserfwesrf

--- js/gift-list.js
+++ js/gift-list.js
@@ -179,6 +179,26 @@
       `;
   };
 
+  const shuffle = (array) => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
   const createGiftList = (container, items, randomize) => {
     if (randomize) {
       shuffle(items);
@@ -236,26 +256,6 @@
       let order = sortInput.val();
       sortByPrice(list, order);
     });
-  };
-
-  const shuffle = (array) => {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
   };
 
   const init = () => {
